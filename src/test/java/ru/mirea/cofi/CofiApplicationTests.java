9db6d0daa89218b670c7968b00f26c525bb5faa0
IAmMum-sAsh <vituchina-natasha11@yandex.ru>
2021-05-05 21:17:45 +0300

    init repo

---
+++ src/test/java/ru/mirea/cofi/CofiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.cofi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
